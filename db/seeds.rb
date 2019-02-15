4bbfc9fea850e3d711856a6a4b3dca41c1ec885a
Andre Antunes <andre@manacacapital.com.br>
2019-02-15 14:19:35 -0200

    cocktail layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: 'Lemon')
+Ingredient.create(name: 'Ice')
+Ingredient.create(name: 'Mint leaves')
+Ingredient.create(name: 'Gin')
+Ingredient.create(name: 'Tonic')
+Ingredient.create(name: 'Rum')
+Ingredient.create(name: 'Cachaca')
+Ingredient.create(name: 'Cointreau')
+Ingredient.create(name: 'Tointreau')
+Ingredient.create(name: 'Ginger')
+Ingredient.create(name: 'Whiskey')
+Ingredient.create(name: 'Demerara Sugar')
+Ingredient.create(name: 'Carbonated Water')
